3c3c400ca6ad3ea7c48c4fa95842240fd354715c
SofiiaDron <dronsofi@gmail.com>
2020-12-01 23:15:12 +0200

    add login

--- models/db.py
+++ models/db.py
@@ -1,12 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from service.app import app
-
 DB_URI = 'postgresql+pg8000://sofii:sofii322@localhost:5432/corporation'
 
-
-def get_db():
-    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-    db = SQLAlchemy(app)
-    db.engine.dialect.description_encoding = None
-    return db
+DB = SQLAlchemy()
--- models/models.py
+++ models/models.py
@@ -1,14 +1,11 @@
 from sqlalchemy import ForeignKey
 
-from db import get_db
+from models.db import DB
 
-db = get_db()
-
-
-class Department(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    dep_name = db.Column(db.String(60), unique=True)
-    head_id =  db.Column(db.Integer, ForeignKey('employee.id'))
+class Department(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    dep_name = DB.Column(DB.String(60), unique=True)
+    head_id = DB.Column(DB.Integer, ForeignKey('employee.id'))
 
     def __init__(self, dep_name, head_id):
         self.dep_name = dep_name
@@ -18,38 +15,45 @@ class Department(db.Model):
     def __repr__(self):
         return '<User %r>' % self.dep_name
 
-class Employee(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    emp_name = db.Column(db.String(60))
-    birthday_date = db.Column(db.Date)
-    position = db.Column(db.String(20))
-    salary = db.Column(db.String(20))
-    dep_id = db.Column(db.Integer, ForeignKey('department.id'))
 
-    def __init__(self, emp_name, birthday_date,position,salary,dep_id):
+class Employee(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    emp_name = DB.Column(DB.String(60))
+    birthday_date = DB.Column(DB.Date)
+    position = DB.Column(DB.String(20))
+    salary = DB.Column(DB.String(20))
+    dep_id = DB.Column(DB.Integer, ForeignKey('department.id'))
+    password = DB.Column(DB.String(60))
+
+    def __init__(self, emp_name, birthday_date,position,salary,dep_id, password):
         self.emp_name = emp_name
         self.birthday_date = birthday_date
         self.position = position
         self.salary = salary
         self.dep_id = dep_id
+        self.password = password
 
     def __repr__(self):
         return '<User %r>' % self.emp_name
 
+def test ():
+    return 123
+
+
 if __name__ == '__main__':
     # run tables creation if it is first run
 
-    # db.create_all()
+    DB.create_all()
 
     # fillup database
 
-    employee1 = Employee('Sofii','2020-12-12','Manager',3000, dep_id=None)
-    employee2 = Employee('Ket','1999-12-12','Engineer',3000, dep_id=None)
-    db.session.add(employee1)
-    db.session.add(employee2)
-    db.session.commit()
+    employee1 = Employee('Sofii','2020-12-12','Manager',3000, dep_id=None,password='sofii')
+    employee2 = Employee('Ket','1999-12-12','Engineer',3000, dep_id=None,password='ket')
+    DB.session.add(employee1)
+    DB.session.add(employee2)
+    DB.session.commit()
 
     dep1 = Department('Lviv', head_id=employee2.id)
-    db.session.add( dep1)
-    db.session.commit()
+    DB.session.add( dep1)
+    DB.session.commit()
 
